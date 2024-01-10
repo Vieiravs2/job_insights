47c22599e4de4991937ce1df3eedc27224e45548
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-01-10 02:25:12 -0300

    Feat: Requisito 05 finalizado

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -7,4 +7,13 @@ class ProcessIndustries(ProcessJobs):
         super().__init__()
 
     def get_unique_industries(self) -> List[str]:
-        pass
+        industries = list()
+        for industry in self.jobs_list:
+            if (
+                industry["industry"] != ""
+                and industry["industry"] not in industries
+            ):
+                industries.append(industry["industry"])
+            
+        return industries
+        
