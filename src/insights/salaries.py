4f1eabd0b8ca544e47a5534eb88865147f08b162
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-01-10 03:05:37 -0300

    Feat: Requisito 06 e 07 finalizados

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -7,10 +7,20 @@ class ProcessSalaries(ProcessJobs):
         super().__init__()
 
     def get_max_salary(self) -> int:
-        pass
-
+        salaries = list()
+        for job in self.jobs_list:
+            if job["max_salary"].isdigit():
+                salaries.append(int(job["max_salary"]))
+        
+        return max(salaries)
+ 
     def get_min_salary(self) -> int:
-        pass
+        salaries = list()
+        for job in self.jobs_list:
+            if job["min_salary"].isdigit():
+                salaries.append(int(job["min_salary"]))
+        
+        return min(salaries)
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
         pass
