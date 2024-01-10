fe4746d56fc57eb115cdae92fe1bd019b2d6996d
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-01-10 02:19:30 -0300

    Feat: Requisito 04 finalizado

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -13,5 +13,12 @@ class ProcessJobs:
     def get_unique_job_types(self) -> List[str]:
         return list(set(job["job_type"] for job in self.jobs_list))
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+    def filter_by_multiple_criteria(self, jobs, criteria) -> List[dict]:
+        jobs_result = list()
+        for job in jobs:
+            if (
+                job["industry"] == criteria["industry"] and
+                job["job_type"] == criteria["job_type"]
+            ):
+                jobs_result.append(job)
+        return jobs_result
