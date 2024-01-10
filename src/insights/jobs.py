2f466fa614f0e4d5f1fa34bb6d4c0605e152dde9
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-01-10 02:00:16 -0300

    Feat: Requisito 02 finalizado

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,12 +1,14 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path) -> List[Dict]:
+        with open(path, "r") as file:
+            self.jobs_list = list(csv.DictReader(file))
 
     def get_unique_job_types(self) -> List[str]:
         pass
